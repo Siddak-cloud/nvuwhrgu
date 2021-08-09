10a942b06de6b32e0eeb6a76b6d4d661669fd2b4
Siddak-cloud <85190641+Siddak-cloud@users.noreply.github.com>
2021-08-09 14:51:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox");
+
+function start() {
+    Textbox.innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run (event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    Textbox.innerHTML = Content;
+    console.log(Content);
+    if (Content=="take my melfie")
+    {
+        console.log("Taking Selfie ------");
+        speak();
+    }
+}
+function speak() {
+    var synth = window.speechSynthesis;
+
+    speak_data= "Taking your selfie in 5 seconds"; 
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000); 
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+ });
+ camera = document.getElementById("camera");
+
+ function take_snapshot() {
+     Webcam.snap(function(data_uri) {
+         document.getElementById("result").innerHTML = '<img id="selfie_img" src="'+data_uri+'">';
+     });
+ }
+ function save() {
+     link = document.getElementById("link");
+     image = document.getElementById("selfie_img").src;
+     link.href = image;
+     link.click();
+ }
